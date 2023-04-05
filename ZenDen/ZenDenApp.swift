c13ba30884c37ecae0241acb715b19170bc8cff3
Olenka Soko <olenkasoko@outlook.com>
2023-04-05 18:21:03 -0400

    Initial Commit

---
+++ ZenDen/ZenDenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZenDenApp.swift
+//  ZenDen
+//
+//  Created by Olenka Soko on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ZenDenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
